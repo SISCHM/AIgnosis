4d85dcd33209534479f7dbb795c5e51fe6fdb9c2
Simon Scherello <simons24@ru.is>
2024-10-23 19:43:13 +0200

    added example logic to app.py

--- DocAnamnesis/app.py
+++ DocAnamnesis/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from utils.functions import process_user_input  # Stelle sicher, dass diese Funktion existiert
-
+from src import Anamnesis
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,6 +15,31 @@ def message():
         response = process_user_input(user_message)  # Bereits implementierte Funktion aufrufen
         return jsonify({'response': response})
     return jsonify({'response': 'Keine Nachricht erhalten.'}), 400
+
+########### Example implementation and how to call it: ####################
+anamnesis = Anamnesis.Anamnesis()
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.json
+    last_question = data.get('last_question')
+    answer = data.get('answer')
+
+    # Process the customer's response and decide the next step
+    result = anamnesis.process_response(last_question, answer)
+
+    # Format and return the response to the web app
+    if result['type'] == 'diagnosis':
+        return jsonify({
+            "message": "Diagnosis",
+            "diagnosis": result['content']
+        })
+    elif result['type'] == 'question':
+        return jsonify({
+            "message": "Next Question",
+            "question": result['content']
+        })
+
+###############################################################
 
 if __name__ == '__main__':
     app.run(debug=True)
